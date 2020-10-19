a3cfcbc757e5a9b616a4ac53c65976428a32a401
unknown <abel.dc.ho@gmail.com>
2020-10-19 15:37:43 +0800

    20201019-1

---
+++ src/App copy.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1>{{ count }}</h1>
+    <h1 v-if="count% 2===0">Even</h1>
+    <button @click="count++">Add</button>
+
+
+    <ol>
+      <li  v-for = "text in list" :key ="text">
+        {{text}}
+      </li> 
+    </ol>
+
+    <input v-model="input"/>
+    <h1>{{ input }}</h1>
+  </div>
+</template>
+ 
+<script>
+export default {
+  data(){
+    return { 
+      count: 0 ,
+      list:[
+        'Learn JavaScript',
+        'Learn Vue',
+        '$pri$'
+,
+
+      ],
+      input:'',
+          methods:{ 
+            add(){
+          this.count++;
+        }
+      }
+    }
+  }
+
+  
+}
+</script>
---
+++ src/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="TodoList">
+    <h1>TodoList</h1>
+    <input v-model="text"/>
+    <ul>
+        <li v-for=" item in list" :key="item">
+             {{item}}
+        </li>
+        <li></li>
+        <li></li>
+    </ul>
+    <button @click="add">Add</button>
+  </div>
+</template>
+<script>
+export default {
+    data: () => ({
+        text:'',
+        list:[],
+
+    }),
+    methods:{
+        add(){
+           this.list.push(this.text); 
+           this.text="";
+        }
+    }
+}
+</script>
